59b403fef81e4572619ed94c9644d565662bebc6
vrroel <alexralv@gmail.com>
2026-03-22 23:17:20 -0300

    add button interaction

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
+const card_btn = document.querySelectorAll(".card_btn");
+console.log(card_btn);
 
-const card_btn2 = document.querySelectorAll('.card_btn');
-console.log(card_btn2);
-
-card_btn2.forEach(btn => {
-    btn.addEventListener('click', agregado)
+card_btn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.textContent = "Agregado";
+    btn.disabled = true;
+  });
 });
-
-function agregado(){
-    alert("Producto agregado al carrito");
-};
